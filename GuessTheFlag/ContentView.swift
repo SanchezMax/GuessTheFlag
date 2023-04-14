1a176a35dd363b4dc74e4fd70d7bc7a90d599a19
Max Zykin <sanchez.max@yandex.ru>
2023-04-14 11:51:42 +0400

    proj3 changes

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -7,6 +7,21 @@
 
 import SwiftUI
 
+struct FlagImage: View {
+    let image: String
+    
+    init(_ image: String) {
+        self.image = image
+    }
+    
+    var body: some View {
+        Image(image)
+            .renderingMode(.original)
+            .clipShape(Capsule())
+            .shadow(radius: 5)
+    }
+}
+
 struct ContentView: View {
     @State private var showingScore = false
     @State private var gameIsFinished = false
@@ -46,10 +61,7 @@ struct ContentView: View {
                         Button {
                             flagTapped(number)
                         } label: {
-                            Image(countries[number])
-                                .renderingMode(.original)
-                                .clipShape(Capsule())
-                                .shadow(radius: 5)
+                            FlagImage(countries[number])
                         }
                     }
                 }
